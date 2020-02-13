33424464054e240e1a1fe12377d7ede031417e90
SANG HYUN SEO <seo.sanghyun@vareal.co.jp>
2020-02-13 14:24:34 +0900

    save image

--- app/controllers/api/save_image_controller.rb
+++ app/controllers/api/save_image_controller.rb
@@ -1,9 +1,12 @@
 module Api
   class SaveImageController < ApplicationController
+
+    # POST "/save"
     def save
-      File.open("tmp/seo/avc_#{Time.zone.now.strftime("%Y%m%d_%H%M%3N")}.png", 'wb') do |f|
+      File.open("tmp/seo/avc_#{Time.zone.now.strftime("%Y%m%d_%H%M%9N")}.png", 'wb') do |f|
         f.write(Base64.decode64(base_64_encoded_data))
       end
+      head :no_content
     end
 
     private
--- app/javascript/packs/join.js
+++ app/javascript/packs/join.js
@@ -1,37 +1,33 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-  navigator.mediaDevices.getUserMedia({ video: true })
-  .then(function (stream) {
-      const video = document.querySelector('#videoElement');
-      const btn = document.querySelector('#btn1');
-      video.srcObject = stream;
-      btn.onclick = function() {
-        // takeSnapshot(video)
-        const take = setInterval(() => takeSnapshot(video), 42);
-        setTimeout(() => clearInterval(take), 3000)
-      }
-    })
-    .catch(function (err0r) {
-      console.log(err0r)
-    });
-});
+  navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    const video = document.querySelector('#videoElement')
+    const btn = document.querySelector('#btn1')
+    video.srcObject = stream
+    btn.onclick = function() {
+      const take = setInterval(() => takeSnapshot(video), 150)
+      setTimeout(() => clearInterval(take), 4000)
+    }
+  }).catch(function (err0r) {
+    console.log(err0r)
+  })
+})
 
 function takeSnapshot(video) {
-  // const img = document.createElement('img');
-  let context;
-  const width = video.offsetWidth;
-  const height = video.offsetHeight;
+  // const img = document.createElement('img')
+  let context
+  const width = video.offsetWidth
+  const height = video.offsetHeight
 
-  canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
+  canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
 
-  context = canvas.getContext('2d');
-  context.drawImage(video, 0, 0);
-  const dataUrl = canvas.toDataURL('image/jpg');
+  context = canvas.getContext('2d')
+  context.drawImage(video, 0, 0)
+  const dataUrl = canvas.toDataURL('image/jpg')
   sendToServer(dataUrl)
-  // img.src = canvas.toDataURL('image/jpg');
-  // document.body.appendChild(img);
+  // img.src = canvas.toDataURL('image/jpg')
+  // document.body.appendChild(img)
 }
 
 // https://stackoverflow.com/questions/7203304/warning-cant-verify-csrf-token-authenticity-rails
@@ -41,6 +37,6 @@ function sendToServer(dataUrl) {
     url: "/save",
     beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
     data: {img: dataUrl},
-    success: () => console.log("done!!")
+    success: () => {console.log("saved")}
   })
 }
