e26d3bcd01345f86070c1422ee78678a9c3d28ef
btpink-seo <btpink.seo@gmail.com>
2020-02-25 11:33:03 +0900

    inception

--- app/controllers/api/save_image_controller.rb
+++ app/controllers/api/save_image_controller.rb
@@ -12,8 +12,8 @@ module Api
           Dir.mktmpdir do |dir|
             File.open("#{dir}/video.webm", 'wb') { |f| f.write(Base64.decode64(base_64_encoded_data)) }
             movie = FFMPEG::Movie.new("#{dir}/video.webm")
-            FileUtils.mkdir_p("tmp/users/#{user_params[:company_id]}")
-            movie.screenshot("tmp/users/#{user_params[:company_id]}/#{Time.zone.now.strftime('%Y%m%d%H%M%S')}_%d.jpg", { vframes: 10000, frame_rate: 24/1, quality: 1 }, validate: false)
+            FileUtils.mkdir_p("lib/assets/python/inception/workspace/users/#{user_params[:company_id]}")
+            movie.screenshot("lib/assets/python/inception/workspace/users/#{user_params[:company_id]}/#{Time.zone.now.strftime('%Y%m%d%H%M%S')}_%d.jpg", { vframes: 10000, frame_rate: 24/1, quality: 1 }, validate: false)
           end
 
           # ML job
--- app/jobs/face_train_job.rb
+++ app/jobs/face_train_job.rb
@@ -6,7 +6,8 @@ class FaceTrainJob < ApplicationJob
     user = User.find(user_id)
 
     # train
-    result = `python lib/assets/python/test.py`
+    train_step = 100
+    result = `python lib/assets/python/inception/retrain.py --bottleneck_dir=./workspace/bottlenecks --model_dir=./workspace/inception --output_graph=./workspace/users_graph.pb --output_labels=./workspace/users_labels.txt --image_dir ./workspace/users --how_many_training_steps #{train_step}`
 
     # send email
     RegistedMailer.with(user: user).registed_email.deliver_now
