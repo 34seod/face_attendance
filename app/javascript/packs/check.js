6c0b7051cf104d6ddafda5b9cec8bd02381577e9
btpink-seo <btpink.seo@gmail.com>
2020-03-11 18:02:26 +0900

    add loading, training ui

--- app/javascript/packs/check.js
+++ app/javascript/packs/check.js
@@ -3,56 +3,53 @@ import { RecordRTCPromisesHandler } from "recordrtc"
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('#face-check')
   const video = document.querySelector('#videoElement')
-  let recorder;
+  let recorder
 
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
     video.srcObject = stream
-    video.play();
-    video.muted = true;
-    recorder = new RecordRTCPromisesHandler(stream, { mimeType: 'video/webm' });
-    btn.onclick = function() {
-      takeSnapshot(recorder, stream)
-    }
-  }).catch(function (err0r) {
-    console.log(err0r)
-  })
+    video.play()
+    video.muted = true
+    recorder = new RecordRTCPromisesHandler(stream, { mimeType: 'video/webm' })
+    btn.onclick = () => takeSnapshot(recorder, stream)
+  }).catch((err0r) => console.log(err0r))
 })
 
 function takeSnapshot(recorder, stream) {
   // Start recording the video
-  recorder.startRecording().then(function() {
-      console.info('Recording video ...');
-  }).catch(function(error) {
-      console.error('Cannot start video recording: ', error);
-  });
+  recorder.startRecording().then(() => {
+      console.info('Recording video ...')
+  }).catch((error) => console.error('Cannot start video recording: ', error))
 
   // release stream on stopRecording
-  recorder.stream = stream;
+  recorder.stream = stream
 
   setTimeout(() => {
-    recorder.stopRecording().then(function() {
-        console.info('stopRecording success');
+    recorder.stopRecording().then(() => {
+      console.info('stopRecording success')
 
-        // Retrieve recorded video as blob and display in the preview element
-        let result = {}
-        recorder.getBlob().then(() => {
-          recorder.getDataURL().then((re) => {
-            result.data = re
-            result.email = $("#email").val()
-            sendToServer(result)
-          })
+      // Retrieve recorded video as blob and display in the preview element
+      let result = {}
+      recorder.getBlob().then(() => {
+        recorder.getDataURL().then((re) => {
+          result.data = re
+          result.email = $("#email").val()
+          sendToServer(result)
         })
-
-        // recorder.stream.stop()
+      })
+      // recorder.stream.stop()
     })
-}, 1500)
+  }, 1500)
 }
 
 function sendToServer(json) {
   $.ajax({
     type: "POST",
     url: "/check",
-    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+      $('.loading-bg').removeClass('d-none')
+    },
+    complete: () => $('.loading-bg').addClass('d-none'),
     data: json,
     success: (res) => {alert(`${res.name} (${res.accurate}%)`)}
   })
--- app/javascript/packs/new.js
+++ app/javascript/packs/new.js
@@ -2,62 +2,56 @@ import { RecordRTCPromisesHandler } from "recordrtc"
 
 // https://github.com/muaz-khan/RecordRTC
 document.addEventListener('DOMContentLoaded', () => {
-    // Store a reference of the preview video element and a global reference to the recorder instance
-    var video = document.getElementById('preview');
-    var recorder;
+  // Store a reference of the preview video element and a global reference to the recorder instance
+  const video = document.getElementById('preview')
+  let recorder
 
-    // When the user clicks on start video recording
-    document.getElementById('face-regist').addEventListener("click", function(){
-        // Request access to the media devices
-        navigator.mediaDevices.getUserMedia({
-            audio: false,
-            video: true
-        }).then(function(stream) {
-            // Display a live preview on the video element of the page
-            video.srcObject = stream;
-
-            // Start to display the preview on the video element
-            // and mute the video to disable the echo issue !
-            video.play();
-            video.muted = true;
-
-            // Initialize the recorder
-            recorder = new RecordRTCPromisesHandler(stream, { mimeType: 'video/webm' });
-
-            // Start recording the video
-            recorder.startRecording().then(function() {
-                console.info('Recording video ...');
-            }).catch(function(error) {
-                console.error('Cannot start video recording: ', error);
-            });
-
-            // release stream on stopRecording
-            recorder.stream = stream;
-        }).then(function() {
-            setTimeout(() => {
-                recorder.stopRecording().then(function() {
-                    console.info('stopRecording success');
-
-                    // Retrieve recorded video as blob and display in the preview element
-                    let result = {}
-                    recorder.getBlob().then((re) => {
-                    }).then(() => {
-                      recorder.getDataURL().then((re) => {
-                        result.data = re
-                      }).then(() => {
-                        result.name = $("#name").val()
-                        result.email = $("#email").val()
-                        sendToServer(result)
-                      })
-                    })
-
-                    recorder.stream.stop()
-                })
-            }, 10000)
-        }).catch(function(error) {
-            console.error("Cannot access media devices: ", error);
-        });
-    }, false);
+  // When the user clicks on start video recording
+  document.getElementById('face-regist').addEventListener("click", () => {
+    // Request access to the media devices
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true }).then((stream) => {
+      // Display a live preview on the video element of the page
+      video.srcObject = stream
+      // Start to display the preview on the video element
+      // and mute the video to disable the echo issue !
+      video.play()
+      video.muted = true
+      // Initialize the recorder
+      recorder = new RecordRTCPromisesHandler(stream, { mimeType: 'video/webm' })
+      // Start recording the video
+      recorder.startRecording()
+              .then(() => console.info('Recording video ...'))
+              .catch((error) => console.error('Cannot start video recording: ', error))
+      // release stream on stopRecording
+      recorder.stream = stream
+    }).then(() => {
+      const recordingTime = 10
+      let intervalTime = recordingTime + 1
+      const interval = setInterval(() => {
+        intervalTime--
+        $("#timer").html(`Recording ${intervalTime}`)
+      }, 1000)
+      setTimeout(() => {
+        clearInterval(interval)
+        $("#timer").html("")
+        $(".recording").addClass("d-none")
+        recorder.stopRecording().then(() => {
+          let result = {}
+          recorder.getBlob().then(() => {
+            recorder.getDataURL().then((re) => {
+              result.data = re
+            }).then(() => {
+              result.name = $("#name").val()
+              result.email = $("#email").val()
+              sendToServer(result)
+            })
+          })
+          console.info('stopRecording success')
+          recorder.stream.stop()
+        })
+      }, recordingTime * 1000)
+    }).catch((error) => console.error("Cannot access media devices: ", error))
+  }, false)
 })
 
 // https://stackoverflow.com/questions/7203304/warning-cant-verify-csrf-token-authenticity-rails
@@ -65,18 +59,25 @@ function sendToServer(json) {
   $.ajax({
     type: "POST",
     url: "/save",
-    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+      $('.loading-bg').removeClass('d-none')
+    },
+    complete: () => $('.loading-bg').addClass('d-none'),
     data: json,
     success: () => {console.log("saved")},
-    error: (res) => {errorHandler(res.responseJSON)}
+    error: (res) => {
+      $('.loading-bg').addClass('d-none')
+      errorHandler(res.responseJSON)
+    }
   })
 }
 // https://jsfiddle.net/9b2e1p0t/2/
 
 function errorHandler(errors) {
-    // 1. close modal
-    $("[data-dismiss=modal]").trigger({ type: "click" });
-    // 2. show error message
-    $("#name_error").text(errors.name || "")
-    $("#email_error").text(errors.email || "")
+  // 1. close modal
+  $("[data-dismiss=modal]").trigger({ type: "click" })
+  // 2. show error message
+  $("#name_error").text(errors.name || "")
+  $("#email_error").text(errors.email || "")
 }
